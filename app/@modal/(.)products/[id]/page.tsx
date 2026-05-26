b78bfa79239de2775617bf22c923811aa99b8ae2
Dalexis Hernandez <yosoydalexis@gmail.com>
2026-05-25 19:38:12 -0500

    Remove product modal interception

--- app/@modal/(.)products/[id]/page.tsx
+++ app/@modal/(.)products/[id]/page.tsx
@@ -1,10 +1,4 @@
-import { cookies } from "next/headers";
-import { notFound } from "next/navigation";
-
-import ProductOverlayModal from "@/components/storefront/ProductOverlayModal";
-import ProductDetailClient from "@/components/storefront/ProductDetailClient";
-import { getStorefrontProductById } from "@/lib/server/catalog";
-import { CLIENT_THEME_COOKIE_KEY, normalizeClientTheme } from "@/lib/shop/client-theme";
+import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
@@ -14,27 +8,5 @@ export default async function ProductOverlayPage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const cookieStore = await cookies();
-  const initialClientTheme = normalizeClientTheme(
-    cookieStore.get(CLIENT_THEME_COOKIE_KEY)?.value,
-    "dark"
-  );
-  const product = await getStorefrontProductById(id);
-
-  if (!product) {
-    notFound();
-  }
-
-  return (
-    <ProductOverlayModal>
-      <ProductDetailClient
-        initialProduct={product}
-        initialReviews={[]}
-        initialRecommended={[]}
-        sessionUser={null}
-        initialClientTheme={initialClientTheme}
-        compact
-      />
-    </ProductOverlayModal>
-  );
+  redirect(`/products/${id}`);
 }
